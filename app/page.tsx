ce4e5df5ea8188a4edb9c93a2172f1d63174e29a
BrainwareUniversity01 <guest.agc.mm@brainwareuniversity.ac.in>
2026-01-05 11:02:45 +0530

    05.01

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
+import Bot from '@/app/components/Bot'
 
 export default function Home() {
   return (
     <>
-      <Link href="/bot">Get started</Link>
+    <Bot/>
       
     </>
   );
